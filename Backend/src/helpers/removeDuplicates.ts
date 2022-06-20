b36908353726054b4e72da106c931711b09037f5
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-06-20 19:50:08 -0300

    removeDuplicate update

--- Backend/src/helpers/removeDuplicates.ts
+++ Backend/src/helpers/removeDuplicates.ts
@@ -2,8 +2,11 @@ import { MovieType } from './convertCsvToJson';
 
 export const removeDuplicates = (array: MovieType[]) => {
   const unique = [];
-  for (let indice = 0; indice < array.length; indice++) {
-    const movie = array[indice];
+
+  const arrayNonUndefined = array.filter((movie: MovieType) => movie.titulo !== 'undefined');
+
+  for (let indice = 0; indice < arrayNonUndefined.length; indice++) {
+    const movie = arrayNonUndefined[indice];
     let isDuplicate = false;
     for (let i = 0; i < unique.length; i++) {
       if (unique[i].titulo === movie.titulo) {
